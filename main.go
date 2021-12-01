1e4f2ce5300cb04e32c0ce843a9105b98ea82cd8
whyrusleeping <why@ipfs.io>
2021-12-01 11:35:19 -0800

    add client, close handler stream when done

--- main.go
+++ main.go
@@ -1,25 +1,35 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"math/rand"
+	"os"
 	"time"
 
 	"github.com/libp2p/go-libp2p"
 	inet "github.com/libp2p/go-libp2p-core/network"
+	"github.com/libp2p/go-libp2p-core/peer"
 )
 
 func main() {
 
+	listener := len(os.Args) == 1
+
 	opts := []libp2p.Option{
-		libp2p.ListenAddrStrings("/ip4/0.0.0.0/tcp/7878"),
 		//libp2p.ConnectionManager(connmgr.NewConnManager(2000, 3000, time.Minute)),
 		//libp2p.Identity(peerkey),
 		//libp2p.BandwidthReporter(bwc),
 		libp2p.DefaultTransports,
 		//libp2p.Transport(libp2pquic.NewTransport),
 	}
+
+	if listener {
+		opts = append(opts, libp2p.ListenAddrStrings("/ip4/0.0.0.0/tcp/7878"))
+	}
+
 	h, err := libp2p.New(opts...)
 	if err != nil {
 		panic(err)
@@ -29,7 +39,35 @@ func main() {
 		fmt.Printf("%s/p2p/%s\n", m, h.ID())
 	}
 
+	if !listener {
+		ai, err := peer.AddrInfoFromString(os.Args[1])
+		if err != nil {
+			panic(err)
+		}
+
+		if err := h.Connect(context.TODO(), *ai); err != nil {
+			panic(err)
+		}
+
+		s, err := h.NewStream(context.TODO(), ai.ID, "/bencher")
+		if err != nil {
+			panic(err)
+		}
+
+		start := time.Now()
+		n, err := io.Copy(ioutil.Discard, s)
+		if err != nil {
+			panic(err)
+		}
+
+		took := time.Since(start)
+
+		fmt.Printf("read %d bytes in %s. (%d bps)\n", n, took, int(float64(n)/took.Seconds()))
+		return
+	}
+
 	h.SetStreamHandler("/bencher", func(s inet.Stream) {
+		defer s.Close()
 		r := rand.New(rand.NewSource(time.Now().UnixNano()))
 		start := time.Now()
 
